1411367169a80086a8b951f4ead410f4a4ae31ab
Pierre <pierrepp92@gmail.com>
2017-03-06 21:53:47 +0100

    init

---
+++ PPMusicImageShadow/PPMusicImageShadow/PPMusicImageShadow.h
@@ -0,0 +1,19 @@
+//
+//  PPMusicImageShadow.h
+//  PPMusicImageShadow
+//
+//  Created by Pierre Perrin on 06/03/2017.
+//  Copyright © 2017 Pierre Perrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PPMusicImageShadow.
+FOUNDATION_EXPORT double PPMusicImageShadowVersionNumber;
+
+//! Project version string for PPMusicImageShadow.
+FOUNDATION_EXPORT const unsigned char PPMusicImageShadowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PPMusicImageShadow/PublicHeader.h>
+
+
