94a65f65cc47d85cdf82763769258d04362cb878
Pierre <pierrepp92@gmail.com>
2017-03-07 10:05:27 +0100

    Mask corrected

---
+++ PPMusicImageShadow/PPMusicImageShadow/PPImageProcessor.swift
@@ -0,0 +1,9 @@
+//
+//  PPImageResizer.swift
+//  PPMusicImageShadow
+//
+//  Created by Pierre Perrin on 07/03/2017.
+//  Copyright © 2017 Pierre Perrin. All rights reserved.
+//
+
+import Foundation
